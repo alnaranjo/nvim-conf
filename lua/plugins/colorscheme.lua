61c5b9954e1d8619aad9b03d919299999a6ccff9
Alvaro Naranjo <alnp@me.com>
2024-08-28 18:27:25 -0400

    Updates keymaps for telescope
    Renames keymaps from <leader>s* to <leader>f* to avoid issues with
    window split keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,17 @@
 -- Priority increased to ensure this gets loaded first
 
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
         local kanagawa = require('kanagawa')
 
         kanagawa.setup({
             dimInactive = true,
-            theme = 'dragon'
+            theme = 'dragon',
         })
 
-		vim.cmd("colorscheme kanagawa-dragon")
-	end,
+        vim.cmd("colorscheme kanagawa-dragon")
+    end,
 }
